c012798ee995ba1534e62bf76b0e5026ffe6a28c
Evgenii-Dolgopolov <do.evgenii@gmail.com>
2024-05-19 21:17:07 +0700

    full styling

--- src/components/pages/Translator.jsx
+++ src/components/pages/Translator.jsx
@@ -5,7 +5,7 @@ export default function Translator() {
     <div className="container">
       <div className="container-frame">
         <div className="input-section">
-          <label className="input-section-title" htmlFor="inputText">
+          <label className="input-section-title-label" htmlFor="inputText">
             Text to Translate:
           </label>
           <textarea
@@ -20,8 +20,69 @@ export default function Translator() {
         </div>
 
         <div className="output-section">
-          <label htmlFor="sourceLanguage">Select language:</label>
+          <div className="output-title-section-container">
+            <label
+              className="output-title-section-label"
+              htmlFor="sourceLanguage">
+              Select language:
+            </label>
+          </div>
 
+          <div className="radio-btn-section">
+            <div className="radio-btns">
+              <input
+                type="radio"
+                id="french"
+                name="language"
+                value="fr"
+                // checked={selectedLanguage === "fr"}
+                // onChange={handleLanguageChange}
+              />
+              <label className="radio-btn-input-label" htmlFor="french">
+                French
+              </label>
+              <img src="../../../public/assets/fr-flag.png" alt="french flag" />
+            </div>
+            <div className="radio-btns">
+              <input
+                type="radio"
+                id="spanish"
+                name="language"
+                value="es"
+                // checked={selectedLanguage === "es"}
+                // onChange={handleLanguageChange}
+              />
+              <label className="radio-btn-input-label" htmlFor="spanish">
+                Spanish
+              </label>
+              <img
+                src="../../../public/assets/sp-flag.png"
+                alt="spanish flag"
+              />
+            </div>
+            <div className="radio-btns">
+              <input
+                type="radio"
+                id="japanese"
+                name="language"
+                value="ja"
+                // checked={selectedLanguage === "ja"}
+                // onChange={handleLanguageChange}
+              />
+              <label className="radio-btn-input-label" htmlFor="japanese">
+                Japanese
+              </label>
+              <img
+                className="img-jpn-flag"
+                src="../../../public/assets/jpn-flag.png"
+                alt="japanese flag"
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="translate-btn-container">
+          <button className="translate-btn">Translate</button>
         </div>
       </div>
     </div>
